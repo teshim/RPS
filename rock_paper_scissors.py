62ef79ae831f84e5e53522e1ff2ac3d56620f62f
Tobey Elliot Shim <tshim24@myth59.stanford.edu>
2023-02-17 17:43:56 -0800

    Added explanation of rules.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,10 @@
 def main():
     print("Hello! Welcome to Rock Paper Scissors: CS45 edition!")
+    print("This is a two player game, with rules as follows:")
+    print("Each player secretly selects one out of 'rock', 'paper', and 'scissors'")
+    print("Then, both players reveal their choice simultaneously, and a winner is decided.")
+    print("Rock beats scissors, scissors beats paper, and paper beats rock.")
+    print("If both players make the same selection, then it's a tie.")
 
 
 if __name__ == "__main__":
