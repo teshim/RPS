74c22db6ea8050f51b6b0bb0d2f8a0dcf1369008
Tobey Elliot Shim <tshim24@myth59.stanford.edu>
2023-02-17 17:45:53 -0800

    Added functionality for user move.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,6 +5,8 @@ def main():
     print("Then, both players reveal their choice simultaneously, and a winner is decided.")
     print("Rock beats scissors, scissors beats paper, and paper beats rock.")
     print("If both players make the same selection, then it's a tie.")
+    print("Let's play! Make your selection: (rock, paper, or scissors)")
+    userMove = input()
 
 
 if __name__ == "__main__":
