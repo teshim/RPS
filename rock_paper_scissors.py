2926ec59a016afc7076c48fb361b7b607b044aae
Tobey Elliot Shim <tshim24@myth59.stanford.edu>
2023-02-17 20:11:20 -0800

    Added first-pass implementation of no-ties feature.
    Added newlines and spaces for increased readability of console output.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,19 +8,20 @@ def main():
     print("Each player secretly selects one out of 'rock', 'paper', and 'scissors'")
     print("Then, both players reveal their choice simultaneously, and a winner is decided.")
     print("Rock beats scissors, scissors beats paper, and paper beats rock.")
-    print("If both players make the same selection, then it's a tie.")
-    print("Let's play! Make your selection: (rock, paper, or scissors)")
-    userMove = input()
-    print("You selected " + userMove + ".")
-    computerMove = random.choice(['rock', 'paper', 'scissors'])
-    print("The computer selected " + computerMove + ".")
-    if computerMove == DEFEATED_BY[userMove]:
-        print("You win!")
-    elif userMove == DEFEATED_BY[computerMove]:
-        print("The computer wins!")
-    else:
-        print("It's a tie!")
+    print("If both players make the same selection, then it's a tie. Let's play!\n")
 
+    while True:
+        userMove = input("Make your selection (rock, paper, or scissors): ")
+        print("You selected " + userMove + ".")
+        computerMove = random.choice(['rock', 'paper', 'scissors'])
+        print("The computer selected " + computerMove + ".")
+        if computerMove == DEFEATED_BY[userMove]:
+            print("You win!")
+            break
+        elif userMove == DEFEATED_BY[computerMove]:
+            print("The computer wins!")
+            break
+        print("It's a tie - play another round!\n")
 
 if __name__ == "__main__":
     main()
